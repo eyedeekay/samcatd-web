1a8458ab023933d9070a4e3adf05c42ae2b03fd4
idk <problemsolver@openmailbox.org>
2018-09-16 10:10:23 -0400

    oh my god even this is more fun than browser shit.

--- strings.go
+++ strings.go
@@ -2,11 +2,18 @@
 
 package samcatweb
 
+type pagestring struct {
+    dir string
+	url  string
+	desc string
+    fields func
+}
+
 var (
-	samcatd_headline       = "samcatd"
-	samcatd_ntcptun        = "ntcp server tunnels"
-	samcatd_httptun        = "http/ntcp server tunnels"
-	samcatd_ssutun         = "ssu server tunnels"
-	samcatd_ntcptun_client = "ntcp client tunnels"
-	samcatd_ssutun_client  = "ssu client tunnels"
+	samcatd_headline       = &pagestring{url: "index", desc: "SAMcatd Control Panel"}
+	samcatd_ntcptun        = &pagestring{url: "ntcpserver", desc: "ntcp server tunnels"}
+	samcatd_httptun        = &pagestring{url: "httpserver", desc: "http/ntcp server tunnels"}
+	samcatd_ssutun         = &pagestring{url: "ssuserver", desc: "ssu server tunnels"}
+	samcatd_ntcptun_client = &pagestring{url: "ntcpclient", desc: "ntcp client tunnels"}
+	samcatd_ssutun_client  = &pagestring{url: "ssuclient", desc: "ssu client tunnels"}
 )
